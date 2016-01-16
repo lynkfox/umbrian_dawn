42feb0203872da096bb97c113115289be41108d3
jglassmaker <jglassmaker@greatwolf.com>
2016-01-15 23:00:56 -0600

    Adding support for german EVE client signature pasting

--- js/core.js
+++ js/core.js
@@ -2065,6 +2065,9 @@ var tripwire = new function() {
 		var rowParse = function(row) {
 			var scanner = {group: "", type: ""};
 			var columns = row.split("	"); // Split by tab
+			var validScanGroups = ["Cosmic Signature", "Cosmic Anomaly", "Kosmische Anomalie", "Kosmische Signatur"];
+			var validGroups = ["Wormhole", "Relic Site", "Gas Site", "Ore Site", "Data Site", "Combat Site",
+								"Wurmloch", "Reliktgebiet", "Gasgebiet", "Mineraliengebiet", "Datengebiet", "Kampfgebiet"];
 
 			for (var x in columns) {
 				if (columns[x].match(/([A-Z]{3}[-]\d{3})/)) {
@@ -2076,12 +2079,12 @@ var tripwire = new function() {
 					continue;
 				}
 
-				if (columns[x] == "Cosmic Signature" || columns[x] == "Cosmic Anomaly") {
+				if ($.inArray(columns[x], validScanGroups) != -1) {
 					scanner.scanGroup = columns[x];
 					continue;
 				}
 
-				if ($.inArray(columns[x], ["Wormhole", "Relic Site", "Gas Site", "Ore Site", "Data Site", "Combat Site"]) != -1) {
+				if ($.inArray(columns[x], validGroups) != -1) {
 					scanner.group = columns[x];
 					continue;
 				}
@@ -2102,20 +2105,26 @@ var tripwire = new function() {
 			var rows = data.split("\n");
 			var data = {"request": {"signatures": {"add": [], "update": []}}};
 			var ids = $.map(tripwire.client.signatures, function(sig) {return viewingSystemID == sig.systemID ? sig.signatureID : sig.sig2ID});
+			var wormholeGroups = ["Wormhole", "Wurmloch"];
+			var siteGroups = ["Combat Site", "Kampfgebiet"];
+			var otherGroups = {"Gas Site": "Gas", "Data Site": "Data", "Relic Site": "Relic", "Ore Site": "Ore",
+								"Gasgebiet": "Gas", "Datengebiet": "Data", "Reliktgebiet": "Relic", "Mineraliengebiet": "Ore"};
 
 			for (var row in rows) {
 				var scanner = rowParse(rows[row]);
 
 				if (scanner.id) {
-					if (scanner.group == "Wormhole") {
+					console.log(scanner)
+					if ($.inArray(scanner.group, wormholeGroups) != -1) {
 						type = "Wormhole";
 						whType = "???";
 						sigName = null;
-					} else if (scanner.group == "Combat Site") {
+					} else if ($.inArray(scanner.group, siteGroups) != -1) {
 						type = 'Sites';
 						sigName = scanner.type;
-					} else if (scanner.group == "Gas Site" || scanner.group == "Data Site" || scanner.group == "Relic Site" || scanner.group == "Ore Site") {
-						type = scanner.group.replace(' Site', '');
+					} else if (otherGroups[scanner.group] /*scanner.group == "Gas Site" || scanner.group == "Data Site" || scanner.group == "Relic Site" || scanner.group == "Ore Site"*/) {
+						//type = scanner.group.replace(' Site', '');
+						type = otherGroups[scanner.group];
 						sigName = scanner.type;
 					} else {
 						type = null;
