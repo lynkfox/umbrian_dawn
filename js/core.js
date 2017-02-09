917d2f5c30946147c8ee31a0f09812ee06e99920
Daimian Mercer <josh.glassmaker@gmail.com>
2017-02-08 22:19:17 -0600

    Remove countdown reference on sig delete

--- js/core.js
+++ js/core.js
@@ -2824,6 +2824,9 @@ var tripwire = new function() {
 			.parent()
 			.find('td > div').animate({backgroundColor: "#4D0000"}, 1000).delay(1000).animate({backgroundColor: "#111"}, 1000)
 			.slideUp(700, function(){
+				// Remove countdown reference
+				$(tr).find('span[data-age]').countdown("destroy");
+
 				$(this).parent().parent().remove();
 				$("#sigTable").trigger("update");
 			});
