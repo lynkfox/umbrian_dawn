f219561235651bdf6cd18fe3dbc3a163b3da2c52
Daimian Mercer <josh.glassmaker@gmail.com>
2017-02-09 14:15:09 -0600

    Fixing the systemChange scope leak issue

--- js/core.js
+++ js/core.js
@@ -2914,6 +2914,123 @@ var tripwire = new function() {
 	// Handles when someone is editing a sig
 	this.sigEditing = function(sig) {}
 
+	// Change the currently viewed system
+	this.systemChange = function(systemID, mode) {
+		if (mode != "init") {
+			$("#infoSecurity").removeClass();
+			$("#infoStatics").empty();
+
+			viewingSystem = tripwire.systems[systemID].name;
+			viewingSystemID = systemID;
+
+			// Reset activity
+			activity.refresh(true);
+
+			// Reset signatures
+			$("#sigTable tbody").empty()
+			tripwire.client.signatures = [];
+
+			// Reset chain map
+			chain.redraw();
+
+			// Reset comments
+			$("#notesWidget .content .comment:visible").remove();
+			tripwire.comments.data = null;
+
+			tripwire.refresh("change");
+		}
+
+		document.title = tripwire.systems[systemID].name + " - " + (server == "static.eve-apps.com" ? "Tripwire" : "Galileo");
+
+		$("#infoSystem").text(tripwire.systems[systemID].name);
+
+		// Current system favorite
+		$.inArray(viewingSystemID, options.favorites) != -1 ? $("#system-favorite").attr("data-icon", "star").addClass("active") : $("#system-favorite").attr("data-icon", "star-empty").removeClass("active");
+
+		if (tripwire.systems[systemID].class) {
+			// Security
+			$("#infoSecurity").html("<span class='wh pointer'>Class " + tripwire.systems[systemID].class + "</span>");
+
+			// Effects
+			if (tripwire.systems[systemID].effect) {
+				var tooltip = "<table cellpadding=\"0\" cellspacing=\"1\">";
+				for (var x in tripwire.effects[tripwire.systems[systemID].effect]) {
+					var effect = tripwire.effects[tripwire.systems[systemID].effect][x].name;
+					var base = tripwire.effects[tripwire.systems[systemID].effect][x].base;
+					var bad = tripwire.effects[tripwire.systems[systemID].effect][x].bad;
+					var whClass = tripwire.systems[systemID].class > 6 ? 6 : tripwire.systems[systemID].class;
+					var modifier = 0;
+
+					switch (Math.abs(base)) {
+						case 15:
+							modifier = base > 0 ? 7 : -7;
+							break;
+						case 30:
+							modifier = base > 0 ? 14 : -14;
+							break;
+						case 60:
+							modifier = base > 0 ? 28 : -28;
+							break;
+					}
+
+					tooltip += "<tr><td>" + effect + "</td><td style=\"padding-left: 25px; text-align: right;\" class=\"" + (bad ? "critical" : "stable") + "\">";
+					tooltip += base + (modifier * (whClass -1)) + "%</td></tr>";
+				}
+				tooltip += "</table>";
+				$("#infoSecurity").append("&nbsp;<span class='pointer' data-tooltip='" + tooltip + "'>" + tripwire.systems[systemID].effect + "</span>");
+				Tooltips.attach($("#infoSecurity [data-tooltip]"));
+			}
+
+			// Statics
+			for (var x in tripwire.systems[systemID].statics) {
+				var type = tripwire.systems[systemID].statics[x];
+				var wormhole = tripwire.wormholes[type];
+				var color = "wh";
+
+				switch (wormhole.leadsTo) {
+					case "High-Sec":
+						color = "hisec";
+						break;
+					case "Low-Sec":
+						color = "lowsec";
+						break;
+					case "Null-Sec":
+						color = "nullsec";
+						break;
+				}
+
+				$("#infoStatics").append("<div><span class='"+ color +"'>&#9679;</span> <b>"+ wormhole.leadsTo +"</b> via <span class='"+ color +"'>"+ type +"</span></div>");
+			}
+
+			// Faction
+			$("#infoFaction").html("&nbsp;");
+		} else {
+			// Security
+			if (tripwire.systems[systemID].security >= 0.45) {
+				$("#infoSecurity").addClass("hisec").text("High-Sec " + tripwire.systems[systemID].security);
+			} else if (tripwire.systems[systemID].security > 0.0) {
+				$("#infoSecurity").addClass("lowsec").text("Low-Sec " + tripwire.systems[systemID].security);
+			} else {
+				$("#infoSecurity").addClass("nullsec").text("Null-Sec " + tripwire.systems[systemID].security);
+			}
+
+			// Faction
+			$("#infoFaction").html(tripwire.systems[systemID].factionID ? tripwire.factions[tripwire.systems[systemID].factionID].name : "&nbsp;");
+		}
+
+		// Region
+		$("#infoRegion").text(tripwire.regions[tripwire.systems[systemID].regionID].name);
+
+		// Info Links
+		$("#infoWidget .infoLink").each(function() {
+			this.href = $(this).data("href").replace(/\$systemName/gi, tripwire.systems[systemID].name).replace(/\$systemID/gi, systemID);
+		});
+
+		// Reset undo/redo
+		tripwire.signatures.undo[systemID] && tripwire.signatures.undo[systemID].length > 0 ? $("#undo").removeClass("disabled") : $("#undo").addClass("disabled");
+		tripwire.signatures.redo[systemID] && tripwire.signatures.redo[systemID].length > 0 ? $("#redo").removeClass("disabled") : $("#redo").addClass("disabled");
+	}
+
 	// Handles data from EVE IGB headers
 	this.EVE = function(EVE, characterChange = false) {
 		var systemChange = this.client.EVE && this.client.EVE.systemChange || false;
@@ -2935,7 +3052,7 @@ var tripwire = new function() {
 
 			// System follower
 			if (!characterChange && options.buttons.follow && (this.client.EVE && this.client.EVE.systemID != EVE.systemID) && $(".ui-dialog:visible").length == 0) {
-				systemChange(EVE.systemID);
+				tripwire.systemChange(EVE.systemID);
 			}
 
 			if (!$("#search").hasClass("active")) {
@@ -3419,8 +3536,8 @@ var tripwire = new function() {
 		this.serverStatus(); // Get TQ status
 		this.pasteSignatures();
 		this.esi();
+		this.systemChange(viewingSystemID, "init");
 		postLoad();
-		systemChange(viewingSystemID, "init");
 	}
 
 	// Use delayed init to speed up rendering
@@ -5644,129 +5761,13 @@ if (window.location.href.indexOf("galileo") != -1) {
 	Notify.trigger("This is the test version of Tripwire.<br/>Please use <a href='https://tripwire.cloud-things.com'>Tripwire</a>")
 }
 
-function systemChange(systemID, mode) {
-	if (mode != "init") {
-		$("#infoSecurity").removeClass();
-		$("#infoStatics").empty();
-
-		viewingSystem = tripwire.systems[systemID].name;
-		viewingSystemID = systemID;
-
-		// Reset activity
-		activity.refresh(true);
-
-		// Reset signatures
-		$("#sigTable tbody").empty()
-		tripwire.client.signatures = [];
-
-		// Reset chain map
-		chain.redraw();
-
-		// Reset comments
-		$("#notesWidget .content .comment:visible").remove();
-		tripwire.comments.data = null;
-
-		tripwire.refresh("change");
-	}
-
-	document.title = tripwire.systems[systemID].name + " - " + (server == "static.eve-apps.com" ? "Tripwire" : "Galileo");
-
-	$("#infoSystem").text(tripwire.systems[systemID].name);
-
-	// Current system favorite
-	$.inArray(viewingSystemID, options.favorites) != -1 ? $("#system-favorite").attr("data-icon", "star").addClass("active") : $("#system-favorite").attr("data-icon", "star-empty").removeClass("active");
-
-	if (tripwire.systems[systemID].class) {
-		// Security
-		$("#infoSecurity").html("<span class='wh pointer'>Class " + tripwire.systems[systemID].class + "</span>");
-
-		// Effects
-		if (tripwire.systems[systemID].effect) {
-			var tooltip = "<table cellpadding=\"0\" cellspacing=\"1\">";
-			for (var x in tripwire.effects[tripwire.systems[systemID].effect]) {
-				var effect = tripwire.effects[tripwire.systems[systemID].effect][x].name;
-				var base = tripwire.effects[tripwire.systems[systemID].effect][x].base;
-				var bad = tripwire.effects[tripwire.systems[systemID].effect][x].bad;
-				var whClass = tripwire.systems[systemID].class > 6 ? 6 : tripwire.systems[systemID].class;
-				var modifier = 0;
-
-				switch (Math.abs(base)) {
-					case 15:
-						modifier = base > 0 ? 7 : -7;
-						break;
-					case 30:
-						modifier = base > 0 ? 14 : -14;
-						break;
-					case 60:
-						modifier = base > 0 ? 28 : -28;
-						break;
-				}
-
-				tooltip += "<tr><td>" + effect + "</td><td style=\"padding-left: 25px; text-align: right;\" class=\"" + (bad ? "critical" : "stable") + "\">";
-				tooltip += base + (modifier * (whClass -1)) + "%</td></tr>";
-			}
-			tooltip += "</table>";
-			$("#infoSecurity").append("&nbsp;<span class='pointer' data-tooltip='" + tooltip + "'>" + tripwire.systems[systemID].effect + "</span>");
-			Tooltips.attach($("#infoSecurity [data-tooltip]"));
-		}
-
-		// Statics
-		for (var x in tripwire.systems[systemID].statics) {
-			var type = tripwire.systems[systemID].statics[x];
-			var wormhole = tripwire.wormholes[type];
-			var color = "wh";
-
-			switch (wormhole.leadsTo) {
-				case "High-Sec":
-					color = "hisec";
-					break;
-				case "Low-Sec":
-					color = "lowsec";
-					break;
-				case "Null-Sec":
-					color = "nullsec";
-					break;
-			}
-
-			$("#infoStatics").append("<div><span class='"+ color +"'>&#9679;</span> <b>"+ wormhole.leadsTo +"</b> via <span class='"+ color +"'>"+ type +"</span></div>");
-		}
-
-		// Faction
-		$("#infoFaction").html("&nbsp;");
-	} else {
-		// Security
-		if (tripwire.systems[systemID].security >= 0.45) {
-			$("#infoSecurity").addClass("hisec").text("High-Sec " + tripwire.systems[systemID].security);
-		} else if (tripwire.systems[systemID].security > 0.0) {
-			$("#infoSecurity").addClass("lowsec").text("Low-Sec " + tripwire.systems[systemID].security);
-		} else {
-			$("#infoSecurity").addClass("nullsec").text("Null-Sec " + tripwire.systems[systemID].security);
-		}
-
-		// Faction
-		$("#infoFaction").html(tripwire.systems[systemID].factionID ? tripwire.factions[tripwire.systems[systemID].factionID].name : "&nbsp;");
-	}
-
-	// Region
-	$("#infoRegion").text(tripwire.regions[tripwire.systems[systemID].regionID].name);
-
-	// Info Links
-	$("#infoWidget .infoLink").each(function() {
-		this.href = $(this).data("href").replace(/\$systemName/gi, tripwire.systems[systemID].name).replace(/\$systemID/gi, systemID);
-	});
-
-	// Reset undo/redo
-	tripwire.signatures.undo[systemID] && tripwire.signatures.undo[systemID].length > 0 ? $("#undo").removeClass("disabled") : $("#undo").addClass("disabled");
-	tripwire.signatures.redo[systemID] && tripwire.signatures.redo[systemID].length > 0 ? $("#redo").removeClass("disabled") : $("#redo").addClass("disabled");
-}
-
 $("body").on("click", "a[href^='.?system=']", function(e) {
 	e.preventDefault();
 
 	var system = $(this).attr("href").replace(".?system=", "");
 	var systemID = Object.index(tripwire.systems, "name", system);
 
-	systemChange(systemID);
+	tripwire.systemChange(systemID);
 });
 
 $("body").on("click", "#undo:not(.disabled)", function() {
