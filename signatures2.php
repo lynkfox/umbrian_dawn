157363a199ab0a0df4050d675d90d9734a3b6ef0
Daimian Mercer <josh.glassmaker@gmail.com>
2017-02-16 15:14:12 -0600

    PHP class work

--- signatures2.php
+++ signatures2.php
@@ -1,12 +1,10 @@
 <?php
 
-//
-
 class signature {
     public $id = 0;
     public $signatureID = '';
     public $systemID = 0;
-    public $type = ['Wormhole', 'Combat', 'Data', 'Relic'];
+    public $type = ['wormhole', 'combat', 'data', 'relic', 'ore', 'gas'];
     public $name = '';
     public $bookmark = '';
     public $lifeTime = date();
@@ -19,6 +17,16 @@ class signature {
     public $modifiedByName = '';
     public $modifiedTime = date();
     public $maskID = 0;
+
+    function __construct($signature) {
+
+        $this->systemID = isset($signature['systemID']) && is_numeric($signature['systemID'])? (int)$signature['systemID'] : 0;
+        $this->type = in_array(strtolower($signature['type']), $this->type) ? $signature['type'] : 'combat';
+        $this->name = isset($signature['name']) && !empty($signature['name']) ? $signature['name'] : null;
+        $this->bookmark = isset($signature['bookmark']) && !empty($signature['bookmark']) ? $signature['bookmark'] : null;
+
+        $this->maskID = isset($signature['maskID']) && is_numeric($signature['maskID'])? (float)$signature['maskID'] : 0;
+    }
 }
 
 class wormhole {
@@ -30,11 +38,25 @@ class wormhole {
     public $mass = ['stable', 'destab', 'critical'];
     public $time = date();
     public $maskID = 0;
+
+    function __construct($wormhole) {
+        $this->type = isset($wormhole['type']) && !empty($wormhole['type']) ? $wormhole['type'] : null;
+        $this->life = in_array(strtolower($wormhole['life']), $this->life) ? $wormhole['life'] : 'stable';
+        $this->mass = in_array(strtolower($wormhole['mass']), $this->mass) ? $wormhole['mass'] : 'stable';
+        $this->maskID = isset($wormhole['maskID']) && is_numeric($wormhole['maskID'])? (float)$wormhole['maskID'] : 0;
+    }
 }
 
+function add($signatures) {
 
-function add() {
-    $signature = new signature();
+    foreach ($signatures as $signature) {
 
+        // check if it is a wormhole or regular signature
+        if (isset($signature['systemID'])) {
+            $signature = new signature($signature);
 
+        } else {
+
+        }
+    }
 }
