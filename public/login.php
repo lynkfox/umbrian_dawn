e778340804e6bd572cb81fc0a939d6b8be611abb
Daimian Mercer <josh.glassmaker@gmail.com>
2018-03-27 15:51:21 -0500

    Refactoring Remember Me feature to be more secure

--- public/login.php
+++ public/login.php
@@ -32,13 +32,37 @@ $mode = 		isset($_REQUEST['mode'])?$_REQUEST['mode']:null;
 $code = 		isset($_REQUEST['code'])?$_REQUEST['code']:null;
 $state =		isset($_REQUEST['state'])?$_REQUEST['state']:null;
 $login =		isset($_REQUEST['login'])?$_REQUEST['login']:null;
+$ip = 			$_SERVER['REMOTE_ADDR'];
 
-if ($mode == 'login' || !$mode) {
-	$username 	= isset($_REQUEST['username'])?$_REQUEST['username']:(isset($_COOKIE['username'])?$_COOKIE['username']:null);
-	$password 	= isset($_REQUEST['password'])?$_REQUEST['password']:(isset($_COOKIE['password'])?$_COOKIE['password']:null);
-	$method		= isset($_REQUEST['username'])?'user':'cookie';
+/**
+ * Generate a random string, using a cryptographically secure
+ * pseudorandom number generator (random_int)
+ *
+ * For PHP 7, random_int is a PHP core function
+ * For PHP 5.x, depends on https://github.com/paragonie/random_compat
+ *
+ * @param int $length      How many characters do we want?
+ * @param string $keyspace A string of all possible characters
+ *                         to select from
+ * @return string
+ */
+function random_str(
+    $length,
+    $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+) {
+    $str = '';
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
+    }
+    return $str;
+}
+
+if ($mode == 'login') {
+	$username 	= isset($_REQUEST['username'])?$_REQUEST['username']:null;
+	$password 	= isset($_REQUEST['password'])?$_REQUEST['password']:null;
+	$method		= 'user';
 	$remember	= isset($_REQUEST['remember'])?1:0;
-	$ip 		= $_SERVER['REMOTE_ADDR'];
 
 	// Check input
 	if (!$username || !$password || !$ip) {
@@ -89,11 +113,7 @@ if ($mode == 'login' || !$mode) {
 					// Log the attempt
 					login_history($ip, $username, $method, 'fail');
 				} else {
-					$query = 'SELECT options FROM preferences WHERE userID = :userID';
-					$stmt = $mysql->prepare($query);
-					$stmt->bindValue(':userID', $account->id, PDO::PARAM_INT);
-					$stmt->execute();
-					$options = json_decode($stmt->fetchColumn(0));
+					$options = json_decode($account->options);
 
 					$_SESSION['userID'] = $account->id;
 					$_SESSION['username'] = $account->username;
@@ -120,8 +140,16 @@ if ($mode == 'login' || !$mode) {
 
 					//save cookie on client PC for 30 days
 					if ($remember) {
-						setcookie('username', $username, time()+60*60*24*30, '/', '', true, true);
-						setcookie('password', $password, time()+60*60*24*30, '/', '', true, true);
+						$token = $hasher->HashPassword(random_str(30));
+						$query = 'INSERT INTO loginToken (userID, token) VALUES (:userID, :token) ON DUPLICATE KEY UPDATE token = :token';
+						$stmt = $mysql->prepare($query);
+						$stmt->bindValue(':userID', $account->id);
+						$stmt->bindValue(':token', $token);
+						$stmt->execute();
+
+						$cookie = base64_encode($account->id . ':' . $token);
+
+						setcookie('tripwire', $cookie, time()+60*60*24*30, '/', '', true, true);
 					}
 				}
 			} else {
@@ -135,7 +163,6 @@ if ($mode == 'login' || !$mode) {
 	}
 } else if ($mode == 'sso') {
 	$method		= 'sso';
-	$ip 		= $_SERVER['REMOTE_ADDR'];
 
 	require('../esi.class.php');
 	$esi = new esi();
@@ -148,11 +175,7 @@ if ($mode == 'login' || !$mode) {
 			$stmt->execute();
 
 			if ($account = $stmt->fetchObject()) {
-				$query = 'SELECT options FROM preferences WHERE userID = :userID';
-				$stmt = $mysql->prepare($query);
-				$stmt->bindValue(':userID', $account->id, PDO::PARAM_INT);
-				$stmt->execute();
-				$options = json_decode($stmt->fetchColumn(0));
+				$options = json_decode($account->options);
 
 				$_SESSION['userID'] = $account->id;
 				$_SESSION['username'] = $account->username;
@@ -167,7 +190,7 @@ if ($mode == 'login' || !$mode) {
 				$_SESSION['options'] = $options;
 
 				// Log the attempt
-				login_history($ip, NULL, $method, 'success');
+				login_history($ip, $account->username, $method, 'success');
 
 				$query = 'INSERT INTO userstats (userID, loginCount) VALUES (:userID, 1) ON DUPLICATE KEY UPDATE lastLogin = NOW(), loginCount = loginCount + 1';
 				$stmt = $mysql->prepare($query);
@@ -216,10 +239,74 @@ if ($mode == 'login' || !$mode) {
 			$esi->login('esi-location.read_online.v1 esi-location.read_location.v1 esi-location.read_ship_type.v1 esi-ui.write_waypoint.v1 esi-ui.open_window.v1', 'evessoesi');
 		}
 	}
-}
+} else if (isset($_COOKIE['tripwire'])) {
+	$method = 'cookie';
 
-if (isset($output['field']) && isset($API) && $output['field'] == 'api') {
-	$output['error'] .= ' Cached Until: ' . $API->cachedUntil .' EVE';
+	// Check login attempts
+	$query = 'SELECT COUNT(ip) FROM _history_login WHERE ip = :ip AND DATE_ADD(time, INTERVAL 30 SECOND) > NOW()';
+	$stmt = $mysql->prepare($query);
+	$stmt->bindValue(':ip', $ip, PDO::PARAM_STR);
+	$stmt->execute();
+	if ($stmt->fetchColumn(0) > 3) {
+		$output['field'] = 'username';
+		$output['error'] = 'Login attempts exceeded, please wait 30 seconds.';
+
+		// Log the attempt
+		login_history($ip, NULL, $method, 'fail');
+	} else {
+		$cookie = explode(':', base64_decode($_COOKIE['tripwire']));
+		$userID = $cookie[0];
+		$token = $cookie[1];
+
+		$query = 'SELECT id, username, password, accounts.ban, characterID, characterName, corporationID, corporationName, admin, super, options, token FROM accounts LEFT JOIN loginToken ON id = loginToken.userID LEFT JOIN preferences ON id = preferences.userID LEFT JOIN characters ON id = characters.userID WHERE id = :userID';
+		$stmt = $mysql->prepare($query);
+		$stmt->bindValue(':userID', $userID);
+		$stmt->execute();
+
+		if ($account = $stmt->fetchObject()) {
+			require('../password_hash.php');
+			$hasher = new PasswordHash(8, FALSE);
+
+			if ($account->ban == 1) {
+				$output['field'] = 'username';
+				$output['error'] = 'You have been banned.';
+
+				// Log the attempt
+				login_history($ip, $account->username, $method, 'fail');
+			} else if ($hasher->CheckPassword($token, $account->token) == false) {
+				$output['field'] = 'username';
+				$output['error'] = 'Remember Me token incorrect.';
+
+				// Log the attempt
+				login_history($ip, $account->username, $method, 'fail');
+			} else {
+				$options = json_decode($account->options);
+
+				$_SESSION['userID'] = $account->id;
+				$_SESSION['username'] = $account->username;
+				$_SESSION['ip'] = $ip;
+				$_SESSION['mask'] = @$options->masks->active ? $options->masks->active : $account->corporationID . '.2';
+				$_SESSION['characterID'] = $account->characterID;
+				$_SESSION['characterName'] = $account->characterName;
+				$_SESSION['corporationID'] = $account->corporationID;
+				$_SESSION['corporationName'] = $account->corporationName;
+				$_SESSION['admin'] = $account->admin;
+				$_SESSION['super'] = $account->super;
+				$_SESSION['options'] = $options;
+
+				$output['result'] = 'success';
+				$output['session'] = $_SESSION;
+
+				// Log the attempt
+				login_history($ip, $account->username, $method, 'success');
+
+				$query = 'INSERT INTO userstats (userID, loginCount) VALUES (:userID, 1) ON DUPLICATE KEY UPDATE lastLogin = NOW(), loginCount = loginCount + 1';
+				$stmt = $mysql->prepare($query);
+				$stmt->bindValue(':userID', $account->id, PDO::PARAM_INT);
+				$stmt->execute();
+			}
+		}
+	}
 }
 
 $output['proccessTime'] = sprintf('%.4f', microtime(true) - $startTime);
