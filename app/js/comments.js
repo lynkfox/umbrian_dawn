073e2d0252c2fdacf4d3870c0bb2db81c9ffb747
Daimian Mercer <josh.glassmaker@gmail.com>
2018-05-03 20:51:29 -0500

    JS fixed for comments

--- app/js/comments.js
+++ app/js/comments.js
@@ -42,8 +42,8 @@ $("body").on("click", ".commentSave, .commentCancel", function(e) {
 			dataType: "JSON"
 		}).done(function(data) {
 			if (data && data.result == true) {
-				$comment.find(".commentModified").html("Edited by " + data.comment.modifiedBy + " at " + data.comment.modifiedDate);
-				$comment.find(".commentCreated").html("Posted by " + data.comment.createdBy + " at " + data.comment.createdDate);
+				$comment.find(".commentModified").html("Edited by " + data.comment.modifiedByName + " at " + data.comment.modifiedDate);
+				$comment.find(".commentCreated").html("Posted by " + data.comment.createdByName + " at " + data.comment.createdDate);
 				Tooltips.attach($comment.find("[data-tooltip]"));
 
 				CKEDITOR.instances[$comment.find(".commentBody").attr("id")].destroy(false);
