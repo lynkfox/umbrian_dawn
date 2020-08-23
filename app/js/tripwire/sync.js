b28b7a81a1cbb55107ae5f0711ddac342c9b9072
Astriania <singularity.syndicate.eve@gmail.com>
2020-08-23 17:35:09 +0100

    Add C13 to autofillable system types (also makes A009 wormholes populate the type correctly)

--- app/js/tripwire/sync.js
+++ app/js/tripwire/sync.js
@@ -24,7 +24,7 @@ tripwire.sync = function(mode, data, successCallback, alwaysCallback) {
         $.extend(this, appData);
 
         this.aSystems = $.map(this.systems, function(system) { return system.name; });
-        this.aSigSystems = ["Null-Sec", "Low-Sec", "High-Sec", "Class-1", "Class-2", "Class-3", "Class-4", "Class-5", "Class-6"];
+        this.aSigSystems = ["Null-Sec", "Low-Sec", "High-Sec", "Class-1", "Class-2", "Class-3", "Class-4", "Class-5", "Class-6", "Class-13"];
         $.merge(this.aSigSystems, this.aSystems.slice());
 
         $(".systemsAutocomplete").inlinecomplete({source: this.aSystems, maxSize: 10, delay: 0});
