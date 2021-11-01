0111992911aaef198db61a707d914f2fa0a50b75
fubbleskag <fubbleskag@gmail.com>
2021-11-01 22:48:13 +0000

    Revert "implements v2/auth. NOTE: SQL structure change required _before_ pushing this to production"
    This reverts commit d60b2db6106b38dfc773bc74e0adc1d1c3dba060.

--- .docker/mysql/tripwire.sql
+++ .docker/mysql/tripwire.sql
@@ -197,7 +197,7 @@ CREATE TABLE `esi` (
   `userID` int(11) NOT NULL,
   `characterID` int(10) unsigned NOT NULL,
   `characterName` varchar(100) NOT NULL,
-  `accessToken` text NOT NULL,
+  `accessToken` varchar(1000) NOT NULL,
   `refreshToken` varchar(1000) NOT NULL,
   `tokenExpire` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`userID`,`characterID`),
@@ -567,18 +567,12 @@ DELIMITER ;;
 /*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;;
 /*!50003 SET @saved_time_zone      = @@time_zone */ ;;
 /*!50003 SET time_zone             = 'SYSTEM' */ ;;
-/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `signatureClean` ON SCHEDULE EVERY 1 MINUTE STARTS '2018-05-11 15:57:18' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
-
-
-
-UPDATE signatures SET modifiedByID = 0, modifiedByName = "Tripwire" WHERE lifeLeft < NOW() AND lifeLength <> '0' AND type <> 'wormhole';
-
-
-
-DELETE FROM signatures WHERE lifeLeft < NOW() AND lifeLength <> '0' AND type <> 'wormhole';
-
-
-
+/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `signatureClean` ON SCHEDULE EVERY 1 MINUTE STARTS '2018-05-11 15:57:18' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+
+UPDATE signatures SET modifiedByID = 0, modifiedByName = "Tripwire" WHERE lifeLeft < NOW() AND lifeLength <> '0' AND type <> 'wormhole';
+
+DELETE FROM signatures WHERE lifeLeft < NOW() AND lifeLength <> '0' AND type <> 'wormhole';
+
 END */ ;;
 /*!50003 SET time_zone             = @saved_time_zone */ ;;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;;
@@ -615,22 +609,14 @@ DELIMITER ;;
 /*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;;
 /*!50003 SET @saved_time_zone      = @@time_zone */ ;;
 /*!50003 SET time_zone             = 'SYSTEM' */ ;;
-/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `wormholeClean` ON SCHEDULE EVERY 1 MINUTE STARTS '2018-05-12 02:24:17' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
-
-
-
-DELETE FROM wormholes WHERE initialID IN (SELECT id FROM signatures WHERE lifeLeft < NOW() AND lifeLength <> 0 AND type = 'wormhole');
-
-
-
-UPDATE signatures SET modifiedByID = 0, modifiedByName = "Tripwire" WHERE lifeLeft < NOW() AND lifeLength <> 0 AND type = 'wormhole';
-
-
-
-DELETE FROM signatures WHERE lifeLeft < NOW() AND lifeLength <> 0 AND type = 'wormhole';
-
-
-
+/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `wormholeClean` ON SCHEDULE EVERY 1 MINUTE STARTS '2018-05-12 02:24:17' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+
+DELETE FROM wormholes WHERE initialID IN (SELECT id FROM signatures WHERE lifeLeft < NOW() AND lifeLength <> 0 AND type = 'wormhole');
+
+UPDATE signatures SET modifiedByID = 0, modifiedByName = "Tripwire" WHERE lifeLeft < NOW() AND lifeLength <> 0 AND type = 'wormhole';
+
+DELETE FROM signatures WHERE lifeLeft < NOW() AND lifeLength <> 0 AND type = 'wormhole';
+
 END */ ;;
 /*!50003 SET time_zone             = @saved_time_zone */ ;;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;;
@@ -649,18 +635,12 @@ DELIMITER ;;
 /*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;;
 /*!50003 SET @saved_time_zone      = @@time_zone */ ;;
 /*!50003 SET time_zone             = 'SYSTEM' */ ;;
-/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `wormholeCritical` ON SCHEDULE EVERY 1 MINUTE STARTS '2014-08-21 03:16:46' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
-
-
-
-UPDATE signatures s INNER JOIN wormholes w ON (s.id = initialID OR s.id = secondaryID) AND life = 'stable' SET modifiedByID = 0, modifiedByName = 'Tripwire', modifiedTime = NOW() WHERE s.type = 'wormhole' AND lifeLength <> '0' AND DATE_SUB(lifeLeft, INTERVAL 4 HOUR) < NOW();
-
-
-
-UPDATE wormholes w INNER JOIN signatures s ON (s.id = initialID OR s.id = secondaryID) AND (s.type = 'wormhole' AND life = 'stable' AND lifeLength <> '0' AND DATE_SUB(lifeLeft, INTERVAL 4 HOUR) < NOW()) SET life = 'critical';
-
-
-
+/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `wormholeCritical` ON SCHEDULE EVERY 1 MINUTE STARTS '2014-08-21 03:16:46' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+
+UPDATE signatures s INNER JOIN wormholes w ON (s.id = initialID OR s.id = secondaryID) AND life = 'stable' SET modifiedByID = 0, modifiedByName = 'Tripwire', modifiedTime = NOW() WHERE s.type = 'wormhole' AND lifeLength <> '0' AND DATE_SUB(lifeLeft, INTERVAL 4 HOUR) < NOW();
+
+UPDATE wormholes w INNER JOIN signatures s ON (s.id = initialID OR s.id = secondaryID) AND (s.type = 'wormhole' AND life = 'stable' AND lifeLength <> '0' AND DATE_SUB(lifeLeft, INTERVAL 4 HOUR) < NOW()) SET life = 'critical';
+
 END */ ;;
 /*!50003 SET time_zone             = @saved_time_zone */ ;;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;;
--- esi.class.php
+++ esi.class.php
@@ -1,226 +1,189 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-
-use Firebase\JWT\JWT;
-use Firebase\JWT\JWK;
-
 class esi {
-	// https://sisilogin.testeveonline.com
-	private static $loginUrl = 'https://login.eveonline.com/v2/oauth';
-	private static $jwksUrl = 'https://login.eveonline.com/oauth/jwks';
-	private static $esiUrl = 'https://esi.evetech.net';
-	public $lastError = null;
-	public $httpCode = null;
-	public $characterID = null;
-	public $characterName = null;
-	public $accessToken = null;
-	public $refreshToken = null;
-	public $tokenExpire = null;
+    // https://sisilogin.testeveonline.com
+    private static $loginUrl = 'https://login.eveonline.com/oauth';
+    private static $esiUrl = 'https://esi.evetech.net';
+    public $lastError = null;
+    public $httpCode = null;
+    public $characterID = null;
+    public $characterName = null;
+    public $accessToken = null;
+    public $refreshToken = null;
+    public $tokenExpire = null;
 
-	private function getAPI($url, $headers = array(), $params = false) {
+    private function getAPI($url, $headers = array(), $params = false) {
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-		if ($params) {
-			curl_setopt($curl, CURLOPT_POST, true);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
-		}
+    if ($params) {
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($params));
+    }
 
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($curl, CURLOPT_USERAGENT, USER_AGENT);
 
 		$result = curl_exec($curl);
-		$this->httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $this->httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-		if ($result === false) {
-			$this->lastError = curl_error($curl);
-		}
+    if ($result === false) {
+        $this->lastError = curl_error($curl);
+    }
 
 		return $result;
 	}
 
-	public function getJWKS() {
-		$result = $this->getAPI(self::$jwksUrl);
+    public function login($scope = NULL, $state = 'evessologin') {
+        $params = array(
+            'response_type' => 'code',
+            'redirect_uri' => EVE_SSO_REDIRECT,
+            'client_id' => EVE_SSO_CLIENT,
+            'scope' => $scope,
+            'state' => $state
+        );
 
-		if ($result === false) {
-			return false;
-		}
+        header('Location: '. self::$loginUrl . '/authorize?' . http_build_query($params), true, 302);
+    }
 
-		$response = json_decode($result);
+    public function authenticate($code) {
+        $headers = array('Authorization: Basic '.base64_encode(EVE_SSO_CLIENT.':'.EVE_SSO_SECRET));
+        $params = array(
+            'grant_type' => 'authorization_code',
+            'code' => $code
+        );
 
-		if (!empty($response)) {
-			foreach ($response->keys as $key) {
-				$jwks['keys'][] = (array) $key;
-			}
-		} else {
-			$this->lastError = 'Could not load JWK data';
-			return false;
-		}
+        $result = $this->getAPI(self::$loginUrl.'/token', $headers, $params);
 
-		return $jwks;
-	}
+        if ($result === false) {
+            return false;
+        }
 
-	public function validateJWT($token) {
-		if ( $jwks = $this->getJWKS() ) {
-			JWT::$leeway = 60;
-			$algs = array_column( $jwks['keys'], 'alg' );
-			$jwt = JWT::decode($token, JWK::parseKeySet($jwks), $algs);
-			$url = parse_url(self::$loginUrl);
-			$issuer = array(
-				$url['host'],
-				$url['scheme']."://".$url['host']
-			);
-			if (!in_array($jwt->iss,$issuer)) {
-				$this->lastError = 'Could not validate the authenticity of the access token';
-				return false;
-			}
-			return $jwt;
-		} else {
-			return false;
-		}
-	}
+        $response = json_decode($result);
+        if (!isset($response->access_token)) {
+            $this->lastError = 'Invalid reponse from ESI during token authentication. ' . $result;
+            return false;
+        }
 
-	public function login($scope = NULL, $state = 'evessologin') {
-		$params = array(
-			'response_type' => 'code',
-			'redirect_uri' => EVE_SSO_REDIRECT,
-			'client_id' => EVE_SSO_CLIENT,
-			'scope' => $scope,
-			'state' => $state
-		);
+        $this->accessToken = $response->access_token;
+        $this->tokenExpire = date('Y-m-d H:i:s', time() + $response->expires_in);
+        $this->refreshToken = $response->refresh_token;
 
-		header('Location: '. self::$loginUrl . '/authorize?' . http_build_query($params), true, 302);
-	}
+        $headers = array('Authorization: Bearer '. $this->accessToken);
 
-	public function authenticate($code) {
-		$headers = array('Authorization: Basic '.base64_encode(EVE_SSO_CLIENT.':'.EVE_SSO_SECRET));
-		$params = array(
-			'grant_type' => 'authorization_code',
-			'code' => $code
-		);
+        $result = $this->getAPI(self::$loginUrl.'/verify', $headers);
 
-		$result = $this->getAPI(self::$loginUrl.'/token', $headers, $params);
+        if ($result === false) {
+            return false;
+        }
 
-		if ($result === false) {
-			return false;
-		}
+        $response = json_decode($result);
+        if (!isset($response->CharacterID)) {
+            $this->lastError = 'Invalid reponse from ESI during verification. ' . $result;
+            return false;
+        }
 
-		$response = json_decode($result);
+        $this->characterID = $response->CharacterID;
+        $this->characterName = $response->CharacterName;
 
-		if (!isset($response->access_token)) {
-			$this->lastError = 'Invalid reponse from ESI during token authentication. ' . $result;
-			return false;
-		}
+        return true;
+    }
 
-		if ( $jwt = $this->validateJWT( $response->access_token ) ) {
-			$this->accessToken = $response->access_token;
-			$this->tokenExpire = date('Y-m-d H:i:s', time() + $response->expires_in);
-			$this->refreshToken = $response->refresh_token;
-			$this->characterID = (int) explode(':', $jwt->sub)[2];
-			$this->characterName = $jwt->name;
-		} else {
-			return false;
-		}
+    public function refresh($refreshToken) {
+        $headers = array('Authorization: Basic '.base64_encode(EVE_SSO_CLIENT.':'.EVE_SSO_SECRET));
+        $params = array(
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $refreshToken
+        );
 
-		return true;
-	}
+        $result = $this->getAPI(self::$loginUrl.'/token', $headers, $params);
 
-	public function refresh($refreshToken) {
-		$headers = array('Authorization: Basic '.base64_encode(EVE_SSO_CLIENT.':'.EVE_SSO_SECRET));
-		$params = array(
-			'grant_type' => 'refresh_token',
-			'refresh_token' => $refreshToken
-		);
+        if ($result === false) {
+            return false;
+        }
 
-		$result = $this->getAPI(self::$loginUrl.'/token', $headers, $params);
+        $response = json_decode($result);
+        if (!isset($response->access_token)) {
+            $this->lastError = 'Invalid reponse from ESI during token refresh. ' . $result;
+            return false;
+        }
 
-		if ($result === false) {
-			return false;
-		}
+        $this->accessToken = $response->access_token;
+        $this->tokenExpire = date('Y-m-d H:i:sP', time() + $response->expires_in);
+        $this->refreshToken = $response->refresh_token;
 
-		$response = json_decode($result);
-		if (!isset($response->access_token)) {
-			$this->lastError = 'Invalid reponse from ESI during token refresh. ' . $result;
-			return false;
-		}
+        return true;
+    }
 
-		$this->accessToken = $response->access_token;
-		$this->tokenExpire = date('Y-m-d H:i:sP', time() + $response->expires_in);
-		$this->refreshToken = $response->refresh_token;
+    public function getCharacter($characterID) {
+        $result = $this->getAPI(self::$esiUrl.'/latest/characters/'.$characterID.'/');
 
-		return true;
-	}
+        if ($result === false || !json_decode($result) || !isset(json_decode($result)->name)) {
+            return false;
+        }
 
-	public function getCharacter($characterID) {
-		$result = $this->getAPI(self::$esiUrl.'/latest/characters/'.$characterID.'/');
+        return json_decode($result);
+    }
 
-		if ($result === false || !json_decode($result) || !isset(json_decode($result)->name)) {
-			return false;
-		}
+    public function getCorporation($corporationID) {
+        $result = $this->getAPI(self::$esiUrl.'/v4/corporations/'.$corporationID.'/');
 
-		return json_decode($result);
-	}
+        if ($result === false || !json_decode($result) || !isset(json_decode($result)->name)) {
+            return false;
+        }
 
-	public function getCorporation($corporationID) {
-		$result = $this->getAPI(self::$esiUrl.'/v4/corporations/'.$corporationID.'/');
+        return json_decode($result);
+    }
 
-		if ($result === false || !json_decode($result) || !isset(json_decode($result)->name)) {
-			return false;
-		}
+    public function getCharacterRoles($characterID) {
+        $headers = array('Authorization: Bearer '. $this->accessToken);
+        $result = $this->getAPI(self::$esiUrl.'/v2/characters/'.$characterID.'/roles/', $headers);
 
-		return json_decode($result);
-	}
+        if ($result === false) {
+            return false;
+        }
 
-	public function getCharacterRoles($characterID) {
-		$headers = array('Authorization: Bearer '. $this->accessToken);
-		$result = $this->getAPI(self::$esiUrl.'/v2/characters/'.$characterID.'/roles/', $headers);
+        return json_decode($result);
+    }
 
-		if ($result === false) {
-			return false;
-		}
+    public function getCharacterTitles($characterID) {
+        $headers = array('Authorization: Bearer '. $this->accessToken);
+        $result = $this->getAPI(self::$esiUrl.'/v1/characters/'.$characterID.'/titles/', $headers);
 
-		return json_decode($result);
-	}
+        if ($result === false) {
+            return false;
+        }
 
-	public function getCharacterTitles($characterID) {
-		$headers = array('Authorization: Bearer '. $this->accessToken);
-		$result = $this->getAPI(self::$esiUrl.'/v1/characters/'.$characterID.'/titles/', $headers);
+        // convert array of objects into just an array of titles
+        $titles = [];
+        foreach (json_decode($result) AS $title) {
+            $titles[] = $title->name;
+        }
 
-		if ($result === false) {
-			return false;
-		}
+        return $titles;
+    }
 
-		// convert array of objects into just an array of titles
-		$titles = [];
-		foreach (json_decode($result) AS $title) {
-			$titles[] = $title->name;
-		}
+    public function getJumps() {
+        $result = $this->getAPI(self::$esiUrl.'/v1/universe/system_jumps/');
 
-		return $titles;
-	}
+        if ($result === false) {
+            return false;
+        }
 
-	public function getJumps() {
-		$result = $this->getAPI(self::$esiUrl.'/v1/universe/system_jumps/');
+        return json_decode($result);
+    }
 
-		if ($result === false) {
-			return false;
-		}
+    public function getKills() {
+        $result = $this->getAPI(self::$esiUrl.'/v2/universe/system_kills/');
 
-		return json_decode($result);
-	}
+        if ($result === false) {
+            return false;
+        }
 
-	public function getKills() {
-		$result = $this->getAPI(self::$esiUrl.'/v2/universe/system_kills/');
-
-		if ($result === false) {
-			return false;
-		}
-
-		return json_decode($result);
-	}
+        return json_decode($result);
+    }
 }
 
 ?>
