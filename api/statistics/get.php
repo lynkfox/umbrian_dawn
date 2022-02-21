543ae51aaccc57e5adc6af5355ef72784ad93cdd
fubbleskag <fubbleskag@gmail.com>
2022-02-21 19:58:44 +0000

    add support for multiple characters

--- api/statistics/get.php
+++ api/statistics/get.php
@@ -1,22 +1,25 @@
 <?php
 
-if (isset($_REQUEST['characterID']) && $maskID) {
-    $query = 'SELECT * FROM statistics WHERE characterID = :characterID AND maskID = :maskID';
-    $stmt = $mysql->prepare($query);
-    $stmt->bindValue(':characterID', $_REQUEST['characterID']);
-    $stmt->bindValue(':maskID', $maskID);
-    $stmt->execute();
-    $rows = $stmt->fetchAll(PDO::FETCH_CLASS);
-    foreach ($rows as $row) {
-      $output[] = $row;
-    }
-} else if ($maskID) {
-    $query = 'SELECT * FROM statistics WHERE maskID = :maskID';
-    $stmt = $mysql->prepare($query);
-    $stmt->bindValue(':maskID', $maskID);
-    $stmt->execute();
-    $rows = $stmt->fetchAll(PDO::FETCH_CLASS);
-    foreach ($rows as $row) {
-      $output[] = $row;
-    }
+$characterIDs = ( isset( $_REQUEST['characterID'] ) ) ? (array) $_REQUEST['characterID'] : [];
+
+if ( ! empty( $characterIDs ) && $maskID ) {
+	$characterIN = implode( ", ", $characterIDs );
+	$query = 'SELECT * FROM statistics WHERE characterID IN ( :characterIN ) AND maskID = :maskID';
+	$stmt = $mysql->prepare( $query );
+	$stmt->bindValue( ':characterIN', $characterIN );
+	$stmt->bindValue( ':maskID', $maskID );
+	$stmt->execute();
+	$rows = $stmt->fetchAll( PDO::FETCH_CLASS );
+	foreach ( $rows as $row ) {
+		$output[] = $row;
+	}
+} else if ( $maskID ) {
+	$query = 'SELECT * FROM statistics WHERE maskID = :maskID';
+	$stmt = $mysql->prepare( $query );
+	$stmt->bindValue( ':maskID', $maskID );
+	$stmt->execute();
+	$rows = $stmt->fetchAll( PDO::FETCH_CLASS );
+	foreach ( $rows as $row ) {
+		$output[] = $row;
+	}
 }
