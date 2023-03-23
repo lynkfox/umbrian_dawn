c3e671610e43aed72470e818e7b7d7bfaa2c18bb
Astriania <singularity.syndicate.eve@gmail.com>
2023-03-23 23:35:59 +0000

    No SDE - Mass looked up client side

--- public/mass.php
+++ public/mass.php
@@ -17,13 +17,13 @@ $wormholeID = $_REQUEST['wormholeID'];
 $maskID = $_SESSION['mask'];
 $output = null;
 
-$query = 'SELECT characterName, toID, shipType, mass, time FROM jumps INNER JOIN '. EVE_DUMP .'.invTypes ON typeID = shipTypeID WHERE maskID = :maskID AND wormholeID = :wormholeID ORDER BY time DESC';
+$query = 'SELECT characterName, toID, shipTypeID, time FROM jumps WHERE maskID = :maskID AND wormholeID = :wormholeID ORDER BY time DESC';
 $stmt = $mysql->prepare($query);
 $stmt->bindValue(':wormholeID', $wormholeID);
 $stmt->bindValue(':maskID', $maskID);
 $stmt->execute();
 
-$output['mass'] = $stmt->fetchAll(PDO::FETCH_CLASS);
+$output['jumps'] = $stmt->fetchAll(PDO::FETCH_CLASS);
 
 $output['proccessTime'] = sprintf('%.4f', microtime(true) - $startTime);
 
