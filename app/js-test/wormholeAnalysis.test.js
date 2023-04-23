1ec3f688da25dfb5e1fb361e18f0b8b62107d7bd
Astriania <singularity.syndicate.eve@gmail.com>
2023-04-23 13:24:28 +0100

    Wormhole type selector - J377 will link to Turnur properly

--- app/js-test/wormholeAnalysis.test.js
+++ app/js-test/wormholeAnalysis.test.js
@@ -14,7 +14,7 @@ describe('Wormhole analysis', () => {
 		it('Specific system', () => { assert.equal(wormholeAnalysis.targetSystemID('J123405', undefined), 31001031); });
 		it('System type in text', () => { assert.equal(wormholeAnalysis.targetSystemID('Low-Sec', undefined), lowsec); });
 		it('System type from wormhole type', () => { assert.equal(wormholeAnalysis.targetSystemID(undefined, 'U210'), lowsec); });
-		it('Specific system from wormhole type', () => { assert.equal(wormholeAnalysis.targetSystemID(undefined, 'J377'), lowsec); });
+		it('Specific system from wormhole type', () => { assert.equal(wormholeAnalysis.targetSystemID(undefined, 'J377'), 30002086); });	// Turnur
 		it('Unknown for K162', () => { assert.equal(wormholeAnalysis.targetSystemID(undefined, 'K162'), null); });
 		it('Unknown for no information', () => { assert.equal(wormholeAnalysis.targetSystemID(undefined, undefined), null); });
 	});
--- app/js/wormholeAnalysis.js
+++ app/js/wormholeAnalysis.js
@@ -4,15 +4,17 @@ wormholeAnalysis = new function() {
 	@param wormholeType The type text of a wormhole e.g. 'U210'
 	@return An ID - either a systemID (31001031), a system type ID (7) or null if there isn't enough information */
 	this.targetSystemID = function(systemText, wormholeType) {
-		if (systemText && Object.index(appData.systems, "name", systemText, true)) {
+		const lookupText = systemText || (appData.wormholes[(wormholeType + '').toUpperCase()] || {}).leadsTo;
+		if(!lookupText) { return null; }
+		if (Object.index(appData.systems, "name", lookupText, true)) {
 			// Leads To is a normal EVE system, so use the sytem ID
-			return Object.index(appData.systems, "name", systemText, true)
+			return Object.index(appData.systems, "name", lookupText, true)
 		} else if (wormholeType && appData.wormholes[wormholeType.toUpperCase()]) {
 			// Leads To can be determined by the wormhole type, so lets use what we know it leads to
-			if (appData.genericSystemTypes.findIndex((item) => appData.wormholes[wormholeType.toUpperCase()].leadsTo.replace(' ', '-').toLowerCase() === item.toLowerCase()) > -1) {
-				return appData.genericSystemTypes.findIndex((item) => appData.wormholes[wormholeType.toUpperCase()].leadsTo.replace(' ', '-').toLowerCase() === item.toLowerCase());
+			if (appData.genericSystemTypes.findIndex((item) => lookupText.replace(' ', '-').toLowerCase() === item.toLowerCase()) > -1) {
+				return appData.genericSystemTypes.findIndex((item) => lookupText.replace(' ', '-').toLowerCase() === item.toLowerCase());
 			}
-		} else if (systemText && appData.genericSystemTypes.findIndex((item) => systemText.toLowerCase() === item.toLowerCase()) !== -1) {
+		} else if (appData.genericSystemTypes.findIndex((item) => lookupText.toLowerCase() === item.toLowerCase()) !== -1) {
 			// Leads To is one of the valid types we allow, so use of of those indexes as reference
 			return appData.genericSystemTypes.findIndex((item) => systemText.toLowerCase() === item.toLowerCase());
 		} else { return null; }
